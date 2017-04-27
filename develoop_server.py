129167a73dd0af745373b8800b18005854147bee
Ceasar Cyrillus <te15ceacyr@falufri.se>
2017-04-27 09:22:21 +0200

    Register and login added

--- develoop_server.py
+++ develoop_server.py
@@ -4,13 +4,35 @@ app = Flask(__name__)
 @app.route("/")
 def home():
 	return render_template("index.html")
+
 #Login for user to be able to start a session on the site
-@app.route("/login", methods=["GET", "POST"])
-def login():
+@app.route("/login_user", methods=["GET", "POST"])
+def login_user():
 	if request.method == "POST":
-		if request.form["password"] == "root":
+		email = request.form["email"]
+		password = request.form["password"]
+		if email == "root" and password == "root":
 			return "userToken"#Return data from database, userToken
 	return "101" #Bad login
 
+#Login page for user interaction
+@app.route("/login")
+def login_page():
+	return render_template("login.html")
+
+#form to register a user
+@app.route("/register")
+def register_page():
+	return render_template("register.html")
+
+@app.route("/register_user", methods=["POST"])
+def register_user():
+	if request.method == "POST":
+		email = request.form["email"]
+		password = request.form["password"]
+		#Database stuff should replace this nonsense
+		#Here is also a good place to place email
+		#check to confirm
+		return "userToken"
 #Runs the app in dev mode
 app.run(host="0.0.0.0", debug=True)
