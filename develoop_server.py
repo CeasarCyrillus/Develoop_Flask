e06a61237aba2fb4715b36320d8ca8f0abf59c2c
Ceasar Cyrillus <te15ceacyr@falufri.se>
2017-04-28 12:25:52 +0200

    Object for responsecodes

--- develoop_server.py
+++ develoop_server.py
@@ -2,28 +2,69 @@ import os
 from flask import *
 from werkzeug.utils import secure_filename
 from random import *
+import json
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "images"
+
+#Response codes
+response_codes = {
+	200:"Successful",
+	400:"Unknown client error",
+	401:"Bad login",
+	402:"Missing file",
+	403:"File not allowed",
+	404:"File extension not allowed",
+	405:"Missing arguments in call",
+	500:"Unknown server error",
+	501:"Method not allowed",
+	502:"Unable to save file"
+	}
+
+#Response object
+class Response():
+	def __init__(self):
+		self.code = 200
+	def status(self):
+		return json.dumps([{"response_code":self.code, "content":response_codes[self.code]}], indent=4)
+
 #register API
-@app.route("/api/register", methods=["POST"])
+@app.route("/api/register", methods=["POST", "GET"])
 def register_api():
+	r = Response()
 	if request.method == "POST":
-		email = request.form["email"]
-		password = request.form["password"]
-		#Database stuff should replace this nonsense
-		#Here is also a good place to place email
-		#check to confirm
-		return "userToken"
+		try:
+			email = request.form["email"]
+			password = request.form["password"]
+			return r.status()
+		except:
+			r.code = 405
+			return r.status()
+	else:
+		r.code = 501
+		return r.status()
+
 
 #Login for user to be able to start a session on the site
-@app.route("/api/login", methods=["POST"])
+@app.route("/api/login", methods=["POST", "GET"])
 def login_api():
+	r = Response()
 	if request.method == "POST":
-		email = request.form["email"]
-		password = request.form["password"]
+		try:
+			email = request.form["email"]
+			password = request.form["password"]
+		except:
+			r.code = 405
+			return r.status()
+
 		if email == "root" and password == "root":
-			return "userToken"#Return data from database, userToken
-	return "101" #Bad login
+			#Return data from database, session token
+			return r.status()
+		else:
+			r.code = 401
+			return r.status()
+	else:
+		r.code = 501
+		return r.status()
 
 #File upload functions
 #Check if the filename is a typical image filename
@@ -38,28 +79,45 @@ def random_name():
 	return ''.join(choice(["1", "2", "3", "A", "B"]) for _ in range(6))
 
 #Upload images api
-@app.route("/api/upload", methods=["POST"])
+@app.route("/api/upload", methods=["POST", "GET"])
 def upload_api():
-	file = request.files["picture"]
-    # Check if the file is one of the allowed types/extensions
+	r = Response()
+	try:
+		file = request.files["picture"]
+	except:
+		r.code = 405
+		return r.status()
+	if file:
+		if allowed_file(file.filename):
+			# Make the filename safe,
+			#remove unsupported chars
+		    filename = secure_filename(file.filename)
 
-	if file and allowed_file(file.filename):
-		# Make the filename safe, remove unsupported chars
-	    filename = secure_filename(file.filename)
+		    #Remember the file extension
+		    file_ext = filename.split(".")[1]
 
-	    #Remember the pictures extension
-	    file_ext = filename.split(".")[1]
-
-	    #Save the file
-	    try:
-	    	new_filename = random_name() + "." + file_ext
-	    	file.save(os.path.join(app.config['UPLOAD_FOLDER'], new_filename))
-	    except:
-	    	return "202"#Could not upload file
+		    #Save the file
+		    try:
+		    	new_filename = random_name() + "." + file_ext
+		    	file.save(os.path.join(app.config['UPLOAD_FOLDER'], new_filename))
+		    except:
+		    	#Could not save file
+		    	r.code = 502
+		    	return r.status()
+		else:
+			#File extension is not allowed
+			r.code = 404
+			return r.status()
 	else:
-			return "201" #Not allowed
+		#File is missing
+		r.code = 402
+		return r.status()
 
-	return "100" #Uploaded file correctly
+#Show the responsecodes used under the api
+@app.route("/api/codes", methods=["POST", "GET"])
+def get_codes():
+	return json.dumps([response_codes])
+
 
 #Runs the app in dev mode
 app.run(host="0.0.0.0", debug=True)
