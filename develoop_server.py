2bdf064e497ad131a12670ead50ee6bf9a2b36b6
Ceasar Cyrillus <te15ceacyr@falufri.se>
2017-04-29 22:15:57 +0200

    REST API

--- develoop_server.py
+++ develoop_server.py
@@ -3,90 +3,81 @@ from flask import *
 from werkzeug.utils import secure_filename
 from random import *
 import json
+import os
+from python_functions import *
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "images"
 
 #Response codes
-response_codes = {
-	200:"Successful",
-	400:"Unknown client error",
-	401:"Bad login",
-	402:"Missing file",
-	403:"File not allowed",
-	404:"File extension not allowed",
-	405:"Missing arguments in call",
-	500:"Unknown server error",
-	501:"Method not allowed",
-	502:"Unable to save file"
-	}
-
-#Response object
-class Response():
-	def __init__(self):
-		self.code = 200
-	def status(self):
-		return json.dumps([{"response_code":self.code, "content":response_codes[self.code]}], indent=4)
+#response_codes = {
+#	200:"OK",
+#	400:"Unknown client error",
+#	401:"Bad login",
+#	402:"Missing file",
+#	403:"File not allowed",
+#	404:"File extension not allowed",
+#	405:"Missing arguments in call",
+#	500:"Unknown server error",
+#	501:"Method not allowed",
+#	502:"Unable to save file"
+#	}
 
 #register API
-@app.route("/api/register", methods=["POST", "GET"])
-def register_api():
+@app.route("/api/register", methods=["POST"])
+def register():
 	r = Response()
+	#Try and create starting folder for user
 	if request.method == "POST":
 		try:
 			email = request.form["email"]
 			password = request.form["password"]
-			return r.status()
+			token = "userToken"
+			create_dir("/images/"+token)
+			return r.status(200, token)
 		except:
-			r.code = 405
-			return r.status()
+			#Could not get arguments
+			return r.status(405)
 	else:
-		r.code = 501
-		return r.status()
+		#Method other than post not allowed
+		return r.status(501)
 
 
 #Login for user to be able to start a session on the site
-@app.route("/api/login", methods=["POST", "GET"])
-def login_api():
+@app.route("/api/login", methods=["POST"])
+def login():
 	r = Response()
 	if request.method == "POST":
 		try:
 			email = request.form["email"]
 			password = request.form["password"]
 		except:
-			r.code = 405
-			return r.status()
-
-		if email == "root" and password == "root":
+			#Could not get arguments in call
+			return r.status(405)
 			#Return data from database, session token
-			return r.status()
+			#This is development evn. rubbish
+			user_in_database = True #Testing purpuses
+			if user_in_database:
+				return r.status(200, "userToken")
 		else:
-			r.code = 401
-			return r.status()
+			#Could not login user
+			return r.status(401)
 	else:
-		r.code = 501
-		return r.status()
-
-#File upload functions
-#Check if the filename is a typical image filename
-def allowed_file(filename):
-	allowed_extensions = set(["png", "jpg"])
-	if filename.split(".")[1].lower() in allowed_extensions:
-		return "." in filename
-	return False
-
-#generate random name for files	
-def random_name():
-	return ''.join(choice(["1", "2", "3", "A", "B"]) for _ in range(6))
+		#No other method than post is allowed
+		return r.status(501)
 
 #Upload images api
-@app.route("/api/upload", methods=["POST", "GET"])
-def upload_api():
+@app.route("/api/upload", methods=["PUT"])
+def upload():
 	r = Response()
 	try:
 		file = request.files["picture"]
+		token = request.form["token"]
+		if token != "userToken":
+			#Bad login
+			return r.status(401)
 	except:
-		r.code = 405
-		return r.status()
+		#Missing file in call
+		return r.status(402)
 	if file:
 		if allowed_file(file.filename):
 			# Make the filename safe,
@@ -102,19 +93,25 @@ def upload_api():
 		    	file.save(os.path.join(app.config['UPLOAD_FOLDER'], new_filename))
 		    except:
 		    	#Could not save file
-		    	r.code = 502
-		    	return r.status()
+		    	return r.status(502)
 		else:
 			#File extension is not allowed
-			r.code = 404
-			return r.status()
+			return r.status(404)
 	else:
 		#File is missing
-		r.code = 402
+		return r.status(402)
+#Download the users images
+@app.route("/api/images", methods="GET")
+def get_images():
+	r = Response()
+	if request.form["token"] != "userToken":
+		#Bad login
+		return r.status(401)
+	if token == "userToken":
+		r.content = image_urls(token)
 		return r.status()
-
-#Show the responsecodes used under the api
-@app.route("/api/codes", methods=["POST", "GET"])
+#Show the response-codes used under the api
+@app.route("/api/codes", methods=["GET"])
 def get_codes():
 	return json.dumps([response_codes])
 
---
+++ python_functions.py
@@ -0,0 +1,57 @@
+#Response object
+class Response():
+	def __init__(self):
+		self.code = 200
+		self.msg = "OK"
+		self.content = ""
+		self.response_codes = {
+		200:"OK",
+		400:"Unknown client error",
+		401:"Bad login",
+		402:"Missing file",
+		403:"File not allowed",
+		404:"File extension not allowed",
+		405:"Missing arguments in call",
+		500:"Unknown server error",
+		501:"Method not allowed",
+		502:"Unable to save file"
+		}
+	def status(self, new_value=False, content=False):
+		if new_value:#new value should never be 0
+			self.code = new_value
+			self.msg = self.response_codes[self.code]
+			self.content = content
+		else:
+			content = None
+		return json.dumps([{"response_code":self.code, "msg":self.msg, "content":self.content}], indent=4)
+
+#Check if the filename is a typical image filename
+def allowed_file(filename):
+	allowed_extensions = set(["png", "jpg"])
+	if filename.split(".")[1].lower() in allowed_extensions:
+		return "." in filename
+	return False
+
+#generate random name for files	
+def random_name():
+	return ''.join(choice(["1", "2", "3", "A", "B"]) for _ in range(6))
+
+#Check wether the user already uploaded that image
+def unique_image(image_url):
+	image = open(image_url,"rb").read()
+	if image == open(compare_image,"rb").read():
+		pass
+
+#Assert folder creation
+def create_dir(file_path):
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+    	try:
+    		os.makedirs(directory)
+    	except:
+        	return False
+    return True
+
+#Get images
+def image_urls(token):
+	os.listdir(token) # returns list
